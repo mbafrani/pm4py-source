39b215c3291eccb67b4685a1ebc2e5365c0ab878
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-01-18 10:30:14 +0100

    Adapted Dockerfiles

--- tests/test_dockers/linux_current/execute_plain.sh
+++ tests/test_dockers/linux_current/execute_plain.sh
@@ -1,9 +1,9 @@
 cd python37
-docker build --no-cache -t testdockerlinuxpython37develop .
-docker run testdockerlinuxpython37develop bash -c "cd pm4py-source-develop/tests && python execute_tests.py"
-docker run testdockerlinuxpython37develop bash -c "python -c \"import pm4py ; print(pm4py.__version__)\""
+docker build --no-cache -t testdockerlinuxpython37bpmnintegration .
+docker run testdockerlinuxpython37bpmnintegration bash -c "cd pm4py-source-bpmnIntegration2/tests && python execute_tests.py"
+docker run testdockerlinuxpython37bpmnintegration bash -c "python -c \"import pm4py ; print(pm4py.__version__)\""
 cd ..
 cd python36
-docker build --no-cache -t testdockerlinuxpython36develop .
-docker run testdockerlinuxpython36develop bash -c "cd pm4py-source-develop/tests && python execute_tests.py"
-docker run testdockerlinuxpython36develop bash -c "python -c \"import pm4py ; print(pm4py.__version__)\""
+docker build --no-cache -t testdockerlinuxpython36bpmnintegration .
+docker run testdockerlinuxpython36bpmnintegration bash -c "cd pm4py-source-bpmnIntegration2/tests && python execute_tests.py"
+docker run testdockerlinuxpython36bpmnintegration bash -c "python -c \"import pm4py ; print(pm4py.__version__)\""
