220fb71d9534d64da08d9e1e975442f899136b0b
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-01-20 08:04:52 +0100

    Resolving conflict

--- pm4py/objects/conversion/__init__.py
+++ pm4py/objects/conversion/__init__.py
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-from pm4py.objects.conversion import tree_to_petri, bpmn_to_petri, petri_to_bpmn
-=======
-from pm4py.objects.conversion import process_tree
->>>>>>> decisionTreeIntegration
+from pm4py.objects.conversion import process_tree, bpmn_to_petri, petri_to_bpmn
