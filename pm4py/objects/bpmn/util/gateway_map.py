550da974785f735cda9fbcda2a3d91332e2578c4
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-01-22 14:32:10 +0100

    get_gateway_map method

--- pm4py/objects/bpmn/util/gateway_map.py
+++ pm4py/objects/bpmn/util/gateway_map.py
@@ -1,4 +1,24 @@
 def get_gateway_map(bpmn_graph):
+    """
+    Gets the gateway map out of a BPMN diagram
+
+    Parameters
+    ------------
+    bpmn_graph
+        BPMN graph
+
+    Returns
+    -----------
+    gateway_map
+        Map whose:
+        - key is the gateway ID
+        - value is a dictionary containing:
+            - type of the gateway
+            - task that is source of the gateway
+            - decision rules that are added
+    edges_map
+        Map of edge ID to edge object
+    """
     gateway_map = {}
     edges_map = {}
 
@@ -19,22 +39,18 @@ def get_gateway_map(bpmn_graph):
                 if x in edges_map and edges_map[x]["targetRef"] in dg.nodes:
                     node_outgoing.append(dg.nodes[edges_map[x]["targetRef"]])
             incoming_tasks = [x for x in node_incoming if "task" in x["type"].lower()]
-            if len(node_incoming) == 1 and len(incoming_tasks) == 1 and len(node_outgoing) > 1:
-                task_nodes = [x for x in node_outgoing if "task" in x["type"].lower()]
+            task_nodes = [x for x in node_outgoing if "task" in x["type"].lower()]
+            if len(incoming_tasks) == 1 and len(node_outgoing) > 1:
                 gateway_nodes = [x for x in node_outgoing if "gateway" in x["type"].lower()]
                 other_nodes = [x for x in node_outgoing if x not in task_nodes and x not in gateway_nodes]
                 if len(other_nodes) == 0 and task_nodes:
                     if gateway_nodes and len(gateway_nodes) == 1 and len(task_nodes) == 1:
                         gateway_map[n] = {"type": "gateway", "source": incoming_tasks[0]["node_name"], "edges": {}}
                         for task in task_nodes:
-                            gateway_map[n]["edges"][task["node_name"]] = task["incoming"][0]
-                    elif len(gateway_nodes) == 0 and len(task_nodes) > 1:
+                            gateway_map[n]["edges"][task["node_name"]] = {"edge": task["incoming"][0], "rules": []}
+                    elif len(task_nodes) > 1:
                         gateway_map[n] = {"type": "onlytasks", "source": incoming_tasks[0]["node_name"], "edges": {}}
                         for task in task_nodes:
-                            gateway_map[n]["edges"][task["node_name"]] = task["incoming"][0]
+                            gateway_map[n]["edges"][task["node_name"]] = {"edge": task["incoming"][0], "rules": []}
 
-    for n in gateway_map:
-        for t in gateway_map[n]["edges"]:
-            print(t, gateway_map[n]["source"], gateway_map[n]["edges"][t])
-
-    return gateway_map
+    return gateway_map, edges_map
