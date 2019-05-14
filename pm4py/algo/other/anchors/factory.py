aa597f205ec599b91a665d5dc440fa92065fb7dc
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-05-14 15:41:58 +0200

    Anchors classification

--- pm4py/algo/other/anchors/factory.py
+++ pm4py/algo/other/anchors/factory.py
@@ -6,6 +6,27 @@ VERSIONS = {CLASSIC: classic.ClassicAnchorClassification}
 
 
 def apply(log, target, classes, variant=CLASSIC, parameters=None):
+    """
+    Initialize the Anchors classification
+
+    Parameters
+    ------------
+    log
+        Log
+    target
+        Numerical target of the classification
+    classes
+        Classes (of the classification)
+    parameters
+        Parameters of the algorithm
+    variant
+        Variant of the algorithm to use, possible values: classic
+
+    Returns
+    ------------
+    anchors
+        Anchors classifier
+    """
     if parameters is None:
         parameters = {}
 
--- pm4py/algo/other/anchors/interface/anchor_classification.py
+++ pm4py/algo/other/anchors/interface/anchor_classification.py
@@ -4,3 +4,9 @@ class AnchorClassification(object):
 
     def train(self):
         raise Exception("not implemented")
+
+    def predict(self, trace):
+        raise Exception("not implemented")
+
+    def explain(self, trace, threshold=-0.01):
+        raise Exception("not implemented")
