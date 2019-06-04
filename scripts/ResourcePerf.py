e152534f99cf5f9d5406c697e2b8689376c60570
Mahsa Pourbafrani <mahsa.bafrani@pads.rwth-aachen.de>
2019-06-04 15:51:26 +0200

    Commit of the Resource-Activity-Performance graph

---
+++ scripts/ResourcePerf.py
@@ -0,0 +1,212 @@
+
+import numpy as np
+import pandas as pd
+import matplotlib.pylab as plt
+from collections import defaultdict
+from pm4py.objects.log import transform
+from pm4py.objects.log.importer.xes import factory as xes_importer
+from pm4py.objects.log.exporter.csv import factory as csv_exporter
+from pyvis.network import Network
+
+class ResourcePerformace:
+
+    def get_input_file(self):
+        event_log_address = input("Event Log:")
+        log_format = event_log_address.split('.')[-1]
+
+        if str(log_format) == 'csv':
+            event_log = pd.read_csv(event_log_address)
+
+        elif str(log_format) == 'xes':
+            xes_log = xes_importer.import_log(event_log_address)
+            event_log = transform.transform_trace_log_to_event_log(xes_log)
+            csv_exporter.export_log(event_log, "event_log.csv")
+            event_log = pd.read_csv("event_log.csv")
+
+        event_log_attributes = event_log.columns
+
+        return event_log
+
+    def create_matrix_resource(self, event_log):
+
+        event_log['Complete Timestamp'] = pd.to_datetime(event_log['Complete Timestamp'])
+        event_log['Start Timestamp'] = pd.to_datetime(event_log['Start Timestamp'])
+        event_duration = abs(event_log['Complete Timestamp'] - event_log['Start Timestamp'])
+        event_log['Event Duration'] = event_duration
+        # create adjancy matrix of activities
+        resource_matrix = pd.DataFrame(
+            np.zeros(shape=(event_log['Resource'].nunique(), event_log['Resource'].nunique())),
+            columns=event_log['Resource'].unique(), index=event_log['Resource'].unique())
+        temp_log = event_log.sort_values(['Start Timestamp'], ascending=True).groupby('Case ID')
+        trace = {}
+
+        for case, casegroup in temp_log:
+            trace.update({case: casegroup['Resource'].values})
+
+        for key, val in trace.items():
+            i = 0
+            while i < (len(val) - 1):
+                resource_matrix[val[i + 1]][val[i]] += 1
+                i += 1
+
+        return resource_matrix
+
+    def create_matrix(self, event_log):
+        event_log['Complete Timestamp'] = pd.to_datetime(event_log['Complete Timestamp'])
+        event_log['Start Timestamp'] = pd.to_datetime(event_log['Start Timestamp'])
+        event_duration = abs(event_log['Complete Timestamp'] - event_log['Start Timestamp'])
+        event_log['Event Duration'] = event_duration
+        # create adjancy matrix of activities
+        matrix = pd.DataFrame(np.zeros(shape=(event_log['Activity'].nunique(), event_log['Activity'].nunique())),
+                              columns=event_log['Activity'].unique(), index=event_log['Activity'].unique())
+        temp_log = event_log.sort_values(['Start Timestamp'], ascending=True).groupby('Case ID')
+        trace = {}
+
+        for case, casegroup in temp_log:
+            trace.update({case: casegroup['Activity'].values})
+
+        for key, val in trace.items():
+            i = 0
+            while i < (len(val) - 1):
+                matrix[val[i + 1]][val[i]] += 1
+                i += 1
+        return matrix
+
+    def find_resource(self, event_log):
+
+        event_log['Complete Timestamp'] = pd.to_datetime(event_log['Complete Timestamp'])
+        event_log['Start Timestamp'] = pd.to_datetime(event_log['Start Timestamp'])
+        event_duration = abs(event_log['Complete Timestamp'] - event_log['Start Timestamp'])
+        event_log['Event Duration'] = event_duration
+
+        freq_act_res_matrix = pd.DataFrame(
+            np.zeros(shape=(len(event_log['Resource'].unique()), len(event_log['Activity'].unique()))),
+            columns=event_log['Activity'].unique(), index=event_log['Resource'].unique())
+        dur_act_res_matrix = pd.DataFrame(
+            np.zeros(shape=(len(event_log['Resource'].unique()), len(event_log['Activity'].unique()))),
+            columns=event_log['Activity'].unique(), index=event_log['Resource'].unique())
+
+        act_groupy = event_log.groupby('Activity')
+        for name, group in act_groupy:
+            resgroup = group.groupby('Resource')['Event Duration']
+            res_per_act_freq = resgroup.size()
+            res_per_act_sum = resgroup.sum()
+
+            for res in res_per_act_freq.keys():
+                freq_act_res_matrix[name][res] = res_per_act_freq.get(res)
+                if res_per_act_freq.get(res) != 0 and res_per_act_sum.get(res) != 0:
+                    dur_act_res_matrix[name][res] = pd.to_timedelta(
+                        (res_per_act_sum.get(res)) / res_per_act_freq.get(res)).seconds / 3600
+
+        return freq_act_res_matrix, dur_act_res_matrix
+
+    def draw_matrix(self, matrix, freq_act_res_matrix, dur_act_res_matrix):
+        # Build a dataframe with 4 connections
+        matrix[matrix < 0] = 0
+        # g = nx.DiGraph()
+        G = Network()
+
+        matrixt = matrix.T
+
+        for act in matrix.columns:
+            G.add_node(act, shape='box')
+            temp_in = matrix[act]
+            if temp_in.max() != 0:
+                tem_max_in = temp_in.idxmax()
+                G.add_node(tem_max_in, shape='box')
+                G.add_edge(act, tem_max_in)
+
+            temp_out = matrixt[act]
+            if temp_out.max() != 0:
+                tem_max_out = temp_out.idxmax()
+                G.add_node(tem_max_out, shape='box')
+                G.add_edge(tem_max_out, act)
+
+        i = 0
+
+        act_act_res_dict = defaultdict(dict)
+        for ac in freq_act_res_matrix.columns:
+            act_res_dict = defaultdict(list)
+            # if ac == act_name:
+            temp_freq_res = freq_act_res_matrix[ac]
+            temp_dur_res = dur_act_res_matrix[ac]
+            temp_dur_res = temp_dur_res[temp_dur_res != 0]
+            """
+            res_fer = temp_freq_res.idxmax()
+            res_dur = temp_dur_res.idxmin()
+            res_dur_max = temp_dur_res.idxmax()
+            res_dur_var = np.std(temp_dur_res)
+            """
+            res_dur_mean = np.mean(temp_dur_res)
+            res_fer_var = np.std(temp_freq_res)
+            res_fer_mean = np.mean(temp_freq_res)
+
+            for res in temp_dur_res.keys():
+                if temp_dur_res.get(res) >= res_dur_mean + res_dur_mean / 2:
+                    act_res_dict[res].append("bad" + "/" + str(temp_dur_res.get(res)))
+                elif res_dur_mean - res_dur_mean / 2 < temp_dur_res.get(res) < res_dur_mean + res_dur_mean / 2:
+                    act_res_dict[res].append("average" + "/" + str(temp_dur_res.get(res)))
+                elif abs(res_dur_mean - res_dur_mean / 2) >= temp_dur_res.get(res):
+                    act_res_dict[res].append("good" + "/" + str(temp_dur_res.get(res)))
+
+                sum_temp = np.sum(temp_freq_res)
+
+                act_res_dict[res].append(100 * (temp_freq_res.get(res) / np.sum(temp_freq_res)))
+                act_act_res_dict[ac].update(act_res_dict)
+
+        for a, av in act_act_res_dict.items():
+            resources = av.keys()
+            for resource in resources:
+                if len(av.get(resource)) > 1 and av.get(resource)[1] >= 1:
+                    resource_duration = av.get(resource)[0]
+                    resource_size = av.get(resource)[1]
+
+                    if resource_duration.split("/")[0] == "good":
+                        r_color = "#00ff1e"
+                    if resource_duration.split("/")[0] == "average":
+                        r_color = "#cdc9c9"
+                    if resource_duration.split("/")[0] == "bad":
+                        r_color = "#ff0000"
+
+                    r_size = resource_size
+
+                    """
+                    if resource_size == "small":
+                        r_size = 5
+                    if resource_size == "large":
+                        r_size =25
+                    if resource_size == "medium":
+                        r_size = 10
+                    """
+
+                    if r_color == "#cdc9c9":
+                        rb_color = "darkgrey"
+                    elif r_color == "#ff0000":
+                        rb_color = "pink"
+                    elif r_color == "#00ff1e":
+                        rb_color = "palegreen"
+
+                    highlight = {'border': r_color, 'background': r_color}
+                    #                    if resource_size > 1:
+
+                    G.add_node(str(resource) + str(i), labelHighlightBold=True,
+                               title=str(round(resource_size, 2)) + "and" + str(resource_duration),
+                               label=resource,
+                               color={'border': rb_color, 'background': rb_color, 'highlight': highlight}, size=r_size)
+                    G.add_edge(str(resource) + str(i), a)
+
+            i += 1
+
+        G.show_buttons(filter_=['physics', 'nodes', 'edges'])
+
+        G.show("mygraph.html")
+        plt.show()
+        return
+
+
+if __name__ == "__main__":
+    resper = ResourcePerformace()
+    event_log = resper.get_input_file()
+    matrix = resper.create_matrix(event_log)
+    freq_act_res_matrix, dur_act_res_matrix = resper.find_resource(event_log)
+    resper.draw_matrix(matrix, freq_act_res_matrix, dur_act_res_matrix)
